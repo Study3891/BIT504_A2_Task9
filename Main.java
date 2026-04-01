88c378d813625ba45e6b27c58eb41b37f861fccb
Study3891 <EmberSong@myop.ac.nz>
2026-04-02 12:26:43 +1300

    Adding main method to repo
    Done with a manual upload because the integration instructions in the course are out of date and incomplete

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This is a line of text");
+		System.out.println("This is a second line of text");
+		System.out.println("This is a third line of text");
+
+
+	}
+
+}
